d9a18d360edcd6e0029f35eabbab58476c517390
Tristan Pinaudeau <tristan@tic.sh>
2021-06-27 18:04:35 +0200

    feat: update article

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
-rm -r dist
-mkdir -p dist/img
-cp templates/index.html dist/index.html
+outdir=www
+
+rm -r $outdir
+mkdir -p $outdir/img
+cp templates/index.html $outdir/index.html
 
 for post in $(ls posts); do
   pandoc posts/$post/post.md --standalone \
@@ -10,12 +12,12 @@ for post in $(ls posts); do
     --include-in-header="templates/header.html" \
     --css="/style.css" \
     --data-dir="posts/$post" \
-    --output="dist/$post.html"
-  mkdir dist/img/$post
-  cp posts/$post/*.(jpeg|gif|png|html) dist/img/$post
-  cp templates/style.css dist
-  sed -i "s!img src=\".!img src=\"/img/$post/!" dist/$post.html 
-  echo "<li><a href=\"$post.html\">$post</a></li>" >> dist/index.html
+    --output="$outdir/$post.html"
+  mkdir $outdir/img/$post
+  cp posts/$post/*.(jpeg|gif|png|html) $outdir/img/$post
+  cp templates/style.css $outdir
+  sed -i "s!img src=\".!img src=\"/img/$post/!" $outdir/$post.html 
+  echo "<li><a href=\"$post.html\">$post</a></li>" >> $outdir/index.html
 done
 
-echo "</ul></body></html>" >> dist/index.html
+echo "</ul></body></html>" >> $outdir/index.html
