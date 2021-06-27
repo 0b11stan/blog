c8972019207434dd5c879f2d3e81e642f69e50fd
Tristan Pinaudeau <tristan@tic.sh>
2021-06-27 17:27:12 +0200

    debt: remove comments

--- build.sh
+++ build.sh
@@ -5,14 +5,12 @@ mkdir -p dist/img
 cp templates/index.html dist/index.html
 
 for post in $(ls posts); do
-  #pushd posts/$post
   pandoc posts/$post/post.md --standalone \
     --highlight-style="breezedark" \
     --include-in-header="templates/header.html" \
     --css="/style.css" \
     --data-dir="posts/$post" \
     --output="dist/$post.html"
-  #popd
   mkdir dist/img/$post
   cp posts/$post/*.(jpeg|gif|png|html) dist/img/$post
   cp templates/style.css dist
