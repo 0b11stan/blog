56931968d3edda42146dd8440d57fdc2682af5eb
Tristan Pinaudeau <tristan@tic.sh>
2021-06-29 16:43:21 +0200

    feat: add post misc-blog-kiss

--- build.sh
+++ build.sh
@@ -7,20 +7,24 @@ mkdir $outdir
 cp templates/index.html $outdir/index.html
 
 for post in $(ls posts); do
+
   pandoc posts/$post/post.md --standalone \
     --highlight-style="breezedark" \
     --include-in-header="templates/header.html" \
     --css="/style.css" \
     --data-dir="posts/$post" \
     --output="$outdir/$post.html"
+
   mkdir -p $outdir/static/$post
-  # find all local files that are linked in the post
+  cp templates/style.css $outdir
+  echo "<li><a href=\"$post.html\">$post</a></li>" >> $outdir/index.html
+
+  # copy only files that are linked in the post
   static_files=$(sed -n 's!.*\[[^\[]*](./\([^(]*\))!\1!p' posts/$post/post.md)
   for file in $static_files; do cp posts/$post/$file $outdir/static/$post/; done
-  cp templates/style.css $outdir
+
   # replace all internal html links with `./myfile.ext` to `/static/mypost/myfile.ext`
   sed -i "s!\(href\|src\)=\"\./\([^\"]*\)\"!\1=\"/static/$post/\2\"!" $outdir/$post.html
-  echo "<li><a href=\"$post.html\">$post</a></li>" >> $outdir/index.html
 done
 
 echo "</ul></body></html>" >> $outdir/index.html
---
+++ draft/misc-blog-kiss/post.md
@@ -0,0 +1,3 @@
+# Créer un système de blog from scratch en 30 lignes de bash
+
+Create a blog but keep it KISS
